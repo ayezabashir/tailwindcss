165a90e3efbb5f3302ae70d07b0160c1a6f2086b
Ayeza <ayezabashir442@gmail.com>
2024-01-09 15:41:30 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-green-900">
+          Hello this is text in green
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./src/App.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
