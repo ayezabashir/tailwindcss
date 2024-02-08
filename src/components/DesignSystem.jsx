349e3caac8bb62505ab85551344fe5dfe4c66f89
Ayeza <ayezabashir442@gmail.com>
2024-02-08 23:13:02 +0500

    design

--- src/components/DesignSystem.jsx
+++ src/components/DesignSystem.jsx
@@ -6,15 +6,7 @@ const DesignSystem = () => {
                 <h2>This is subtitle</h2>
                 <p>This is a paragraph</p>
                 <a href="#">This is an anchor</a>
-                <div className="my-4">
-                    <button className="btn btn-primary">Primary button</button>
-                </div>
-                <div className="my-4">
-                    <button className="btn btn-secondary">Secondary button</button>
-                </div>
-                <div className="my-4">
-                    <button disabled className="btn" >Disabled button</button>
-                </div>
+
                 <div>
                     <input type="text" placeholder="email" />
                 </div>
